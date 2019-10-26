21307d94033dd0c8dd2d1a3fb8149dceb3cb7a93
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-10-25 21:59:22 -0300

    Definindo o header da classe criança.

--- inc/crianca.h
+++ inc/crianca.h
@@ -1,4 +1,14 @@
 #ifndef CRIANCA_H
 #define CRIANCA_H
 
+#include <usuario.h>
+
+class Crianca : public Usuario {
+    private:
+        int responsavel;
+    public:
+        int get_responsavel();
+        void set_responsavel(int r);
+}
+
 #endif//CRIANCA_H
