3484586fd0f6ad6defed3524c64aa659f26e72cc
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-10-25 21:48:53 -0300

    Definindo o header da classe adulto.

--- inc/adulto.h
+++ inc/adulto.h
@@ -1,4 +1,16 @@
 #ifndef ADULTO_H
 #define ADULTO_H
 
+#include <usuario.h>
+#include <vector>
+
+class Adulto : public Usuario {
+    private:
+        vector<int> dependentes;
+    public:
+        vector<int> get_dependentes();
+        void add_dependente(int d);
+        void remove_dependente(int d);
+}
+
 #endif//ADULTO_H
