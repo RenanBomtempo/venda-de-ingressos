9e6e0263865c7f06e11d44a2d2428dad020943c2
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-10-26 01:26:00 -0300

    Header e implementação da classe evento adulto.

--- inc/eventoadulto.h
+++ inc/eventoadulto.h
@@ -1,4 +1,14 @@
 #ifndef EVENTO_ADULTO_H
 #define EVENTO_ADULTO_H
 
+using namespace std;
+
+class EventoAdulto{
+    private:
+        int quota_idoso;
+    public:
+        EventoAdulto(int i, string n, Usuario* d, int c, int q);
+        EventoAdulto(int i, string n, Usuario* d, int c, vector<int> ca, vector<int> p, int q);
+}
+
 #endif//EVENTO_ADULTO_H
--- src/eventoadulto.cpp
+++ src/eventoadulto.cpp
@@ -1,1 +1,14 @@
 #include "eventoadulto.h"
+
+using namespace std;
+
+EventoAdulto::EventoAdulto(int i, string n, Usuario* d, int c, int q) :
+                                                Evento(i, n, d, c){
+    quota_idoso = q;
+}
+
+EventoAdulto::EventoAdulto(int i, string n, Usuario* d, int c,
+                           vector<int> ca, vector<int> p, int q) :
+                                                Evento(i, n, d, c, ca, p){
+    quota_idoso = q;
+}
