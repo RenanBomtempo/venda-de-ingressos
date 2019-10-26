35eb8e8a8da60475659eb499e44143d7d7f6da6d
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-10-26 02:07:04 -0300

    Header e implementação de teatro fantoche

--- inc/teatrofantoche.h
+++ inc/teatrofantoche.h
@@ -1,4 +1,20 @@
 #ifndef TEATRO_FANTOCHE_H
 #define TEATRO_FANTOCHE_H
 
+#include <eventoinfantil.h>
+
+using namespace std;
+
+class TeatroFantoche : public EventoInfantil{
+    private:
+        vector<int> horarios;
+    public:
+        TeatroFantoche(int i, string n, Usuario* d, int c);
+        TeatroFantoche(int i, string n, Usuario* d, int c,
+                       vector<int> ca, vector<int> p);
+        TeatroFantoche(int i, string n, Usuario* d, int c,
+                       vector<int> ca, vector<int> p,
+                       vector<int> h);
+}
+
 #endif//TEATRO_FANTOCHE_H
--- src/teatrofantoche.cpp
+++ src/teatrofantoche.cpp
@@ -1,1 +1,23 @@
 #include "teatrofantoche.h"
+
+using namespace std;
+
+TeatroFantoche::TeatroFantoche(int i, string n, Usuario* d, int c) :
+                                            EventoInfantil(i, n, d, c){
+}
+
+TeatroFantoche::TeatroFantoche(int i, string n, Usuario* d, int c,
+                               vector<int> ca, vector<int> p) :
+                                            EventoInfantil(i, n, d, c, ca, p){
+}
+
+TeatroFantoche::TeatroFantoche(int i, string n, Usuario* d, int c,
+                               vector<int> ca, vector<int> p,
+                               vector<int> h) :
+                                            EventoInfantil(i, n, d, c, ca, p){
+    horarios = h;
+}
+
+void TeatroFantoche::add_horario(int h){
+    horarios.push_back(h);
+}
