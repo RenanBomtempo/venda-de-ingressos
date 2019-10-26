4abc8f585160b894fbd6aefc225502569bb074a1
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-10-26 00:53:20 -0300

    Mudança de tipo de dependentes em adulto (Crianca -> Crianca*)

--- inc/adulto.h
+++ inc/adulto.h
@@ -2,21 +2,20 @@
 #define ADULTO_H
 
 #include <usuario.h>
-#include <crianca.h>
+#include <initialize_classes.h>
 #include <vector>
 
 using namespace std;
 
 class Adulto : public Usuario {
     private:
-        vector<Crianca> dependentes;
+        vector<Crianca*> dependentes;
     public:
         Adulto();
         Adulto(int i, string n, int idad, float s);
         Adulto(int i, string n, int idad, float s, int t);
-        vector<Crianca> get_dependentes();
-        void add_dependente(Crianca d);
-        void remove_dependente(int id_d);
+        vector<Crianca*> get_dependentes();
+        void add_dependente(Crianca* d);
 };
 
 #endif//ADULTO_H
--- src/adulto.cpp
+++ src/adulto.cpp
@@ -11,18 +11,10 @@ Adulto::Adulto(int i, string n, int idad, float s) : Usuario(i, n, idad, s, 1){
 Adulto::Adulto(int i, string n, int idad, float s, int t)  : Usuario(i, n, idad, s, t){
 }
 
-vector<Crianca> Adulto::get_dependentes(){
+vector<Crianca*> Adulto::get_dependentes(){
     return dependentes;
 }
 
-void Adulto::add_dependente(Crianca d){
+void Adulto::add_dependente(Crianca* d){
     dependentes.push_back(d);
-}
-
-void Adulto::remove_dependente(int id_d){
-    for (int i = 0; i < dependentes.size(); i++){
-        if (dependentes[i].get_id() == id_d){
-            dependentes.erase(dependentes.begin()+i);
-        }
-    }
 }
