93234394d4a2a23e1f1cbe54f336ea50c26f0df5
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-10-26 02:07:20 -0300

    Correção de include.

--- inc/eventoinfantil.h
+++ inc/eventoinfantil.h
@@ -2,14 +2,14 @@
 #define EVENTO_INFANTIL_H
 
 #include <evento.h>
-#include <usuario.h>
 
 using namespace std;
 
 class EventoInfantil : public Evento{
     public:
         EventoInfantil(int i, string n, Usuario* d, int c);
-        EventoInfantil(int i, string n, Usuario* d, int c, vector<int> ca, vector<int> p);
+        EventoInfantil(int i, string n, Usuario* d, int c,
+                       vector<int> ca, vector<int> p);
 }
 
 #endif//EVENTO_INFANTIL_H
