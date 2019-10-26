7229d5798d855766f668f23c99653dcbea77940e
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-10-26 01:19:07 -0300

    Header e implementação da classe evento.

--- inc/evento.h
+++ inc/evento.h
@@ -1,4 +1,24 @@
 #ifndef EVENTO_H
 #define EVENTO_H
 
+#include <string>
+#include <Usuario>
+
+using namespace std;
+
+class Evento{
+    private:
+        int id;
+        string nome;
+        Usuario* dono;
+        int categoria;
+        vector<int> capacidades;
+        vector<int> precos;
+    public:
+        Evento(int i, string n, Usuario* d, int c);
+        Evento(int i, string n, Usuario* d, int c, vector<int> ca, vector<int> p);
+        void add_ingresso(int c, int p);
+
+}
+
 #endif//EVENTO_H
--- src/evento.cpp
+++ src/evento.cpp
@@ -1,1 +1,20 @@
 #include "evento.h"
+
+using namespace std;
+
+Evento::Evento(int i, string n, Usuario* d, int c){
+    id = i;
+    nome = n;
+    dono = d;
+    categoria = c;
+}
+
+Evento::Evento(int i, string n, Usuario* d, int c, vector<int> ca, vector<int> p) : Evento(i, n, d, c){
+    capacidades = ca;
+    precos = p;
+}
+
+Evento::add_ingresso(int c, int p){
+    capacidades.push_back(c);
+    precos.push_back(p);
+}
