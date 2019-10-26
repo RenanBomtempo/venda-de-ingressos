6a1c2930727ef76837cce3af446cbf7d84c5812d
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-10-25 22:46:45 -0300

    Mudança de vector pra set em dependentes de adultos.

--- inc/adulto.h
+++ inc/adulto.h
@@ -2,17 +2,17 @@
 #define ADULTO_H
 
 #include <usuario.h>
-#include <vector>
+#include <set>
 
 using namespace std;
 
 class Adulto : public Usuario {
     private:
-        vector<int> dependentes;
+        set<int> dependentes;
     public:
         Adulto(int i, string n, int idad, float s)  : Usuario(i, n, idad, s, 1);
         Adulto(int i, string n, int idad, float s, int t)  : Usuario(i, n, idad, s, t);
-        vector<int> get_dependentes();
+        set<int> get_dependentes();
         void add_dependente(int d);
         void remove_dependente(int d);
 }
