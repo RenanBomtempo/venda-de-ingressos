3762d4d1685c89c6a41da413009a2dbba321ae33
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-10-26 00:31:17 -0300

    Header de definição vazia das classes para corrigir interdependencia.

---
+++ inc/initialize_classes.h
@@ -0,0 +1,12 @@
+class Usuario;
+    class Crianca;
+    class Adulto;
+        class Idoso;
+
+class Evento;
+    class Cinema;
+    class EventoInfantil;
+        class TeatroFantoche;
+    class EventoAdulto;
+        class Boate;
+        class Show;
