95db13210e34179dd6b828f59453e5256af612c5
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-10-26 01:32:03 -0300

    Header e implementação da classe evento infantil.

--- inc/eventoinfantil.h
+++ inc/eventoinfantil.h
@@ -1,4 +1,15 @@
 #ifndef EVENTO_INFANTIL_H
 #define EVENTO_INFANTIL_H
 
+#include <evento.h>
+#include <usuario.h>
+
+using namespace std;
+
+class EventoInfantil : public Evento{
+    public:
+        EventoInfantil(int i, string n, Usuario* d, int c);
+        EventoInfantil(int i, string n, Usuario* d, int c, vector<int> ca, vector<int> p);
+}
+
 #endif//EVENTO_INFANTIL_H
--- src/eventoinfantil.cpp
+++ src/eventoinfantil.cpp
@@ -1,1 +1,10 @@
 #include "eventoinfantil.h"
+
+EventoInfantil::EventoInfantil(int i, string n, Usuario* d, int c) :
+                                                Evento(i, n, d, c){
+}
+
+Evento::Evento(int i, string n, Usuario* d, int c,
+               vector<int> ca, vector<int> p) :
+                                                Evento(i, n, d, c, ca, p){
+}
