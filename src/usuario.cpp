176495a342f03c463d8b18348d6d775616b510da
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-10-25 23:03:19 -0300

    Correções da implementação da classe usuario.

--- src/usuario.cpp
+++ src/usuario.cpp
@@ -2,21 +2,6 @@
 
 using namespace std;
 
-class Usuario {
-    private:
-        int id;
-        string nome;
-        int idade;
-        float saldo;
-        int tipo;
-    public:
-        int get_id();
-        string get_nome();
-        int get_idade();
-        float get_saldo();
-        int get_tipo();
-}
-
 Usuario::Usuario(int i, string n, int idad, float s, int t){
     id = i;
     nome = n;
@@ -25,22 +10,22 @@ Usuario::Usuario(int i, string n, int idad, float s, int t){
     tipo = t;
 }
 
-Usuario::get_id(){
+int Usuario::get_id(){
     return id;
 }
 
-Usuario::get_nome(){
+string Usuario::get_nome(){
     return nome;
 }
 
-Usuario::get_idade(){
+int Usuario::get_idade(){
     return idade;
 }
 
-Usuario::get_saldo(){
+float Usuario::get_saldo(){
     return saldo;
 }
 
-Usuario::get_tipo(){
+int Usuario::get_tipo(){
     return tipo;
 }
