8a7326dd6967929cfeae1d57f46cb254a42e5e9e
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-10-26 01:51:16 -0300

    Header e implementação de show.

--- inc/show.h
+++ inc/show.h
@@ -1,4 +1,26 @@
 #ifndef SHOW_H
 #define SHOW_H
 
+#include <eventoadulto.h>
+#include <usuario.h>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Show : public EventoAdulto{
+    private:
+        int abertura_portoes;
+        vector<string> artistas;
+    public:
+        Show(int i, string n, Usuario* d, int c, int q, int ap);
+        Show(int i, string n, Usuario* d, int c,
+             vector<int> ca, vector<int> p, int q,
+             int ap)
+        Show(int i, string n, Usuario* d, int c,
+             vector<int> ca, vector<int> p, int q,
+             int ap, vector<string> ar);
+        void add_artista(string ar);
+}
+
 #endif//SHOW_H
--- src/show.cpp
+++ src/show.cpp
@@ -1,1 +1,26 @@
 #include "show.h"
+
+using namespace std;
+
+Show::Show(int i, string n, Usuario* d, int c, int q, int ap) :
+                                            EventoAdulto(i, n, d, c, q){
+    abertura_portoes = ap;
+}
+
+Show::Show(int i, string n, Usuario* d, int c,
+           vector<int> ca, vector<int> p, int q,
+           int ap) :
+                                            EventoAdulto(i, n, d, c, ca, p, q){
+    abertura_portoes = ap;
+}
+
+Show::Show(int i, string n, Usuario* d, int c,
+           vector<int> ca, vector<int> p, int q,
+           int ap, vector<string> ar) :
+                                            Show(i, n, d, c, ca, p, q, ap){
+    artistas = ar;
+}
+
+void Show::add_artista(string ar){
+    artistas.push_back(ar);
+}
