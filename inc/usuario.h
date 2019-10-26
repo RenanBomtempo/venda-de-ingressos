71db4989131f4fc6b0f3c904a4c54f020774fd7a
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-10-25 21:48:43 -0300

    Definindo o header da classe usuário

--- inc/usuario.h
+++ inc/usuario.h
@@ -1,4 +1,22 @@
 #ifndef USUARIO_H
 #define USUARIO_H
 
+#include <string>
+
+class Usuario {
+    private:
+        int id;
+        string nome;
+        int idade;
+        float saldo;
+        int tipo;
+    public:
+        Usuario(int i, string n, int idad, float s, int t);
+        int get_id();
+        string get_nome();
+        int get_idade();
+        float get_saldo();
+        int get_tipo();
+}
+
 #endif//USUARIO_H
