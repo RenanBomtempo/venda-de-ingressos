1123e3fe70f7591aa622ef4e1e1e37b68678ad58
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-10-26 02:00:41 -0300

    Header e implementação de cinema.

--- inc/cinema.h
+++ inc/cinema.h
@@ -1,4 +1,23 @@
 #ifndef CINEMA_H
 #define CINEMA_H
 
+#include <evento.h>
+#include <usuario.h>
+
+using namespace std;
+
+class Cinema : public Evento{
+    private:
+        vector<int> horarios;
+        int duracao;
+    public:
+        Cinema(int i, string n, Usuario* d, int c, int d);
+        Cinema(int i, string n, Usuario* d, int c,
+               vector<int> ca, vector<int> p, int d);
+        Cinema(int i, string n, Usuario* d, int c,
+               vector<int> ca, vector<int> p, int d,
+               vector<int> h);
+        void add_horario(int h);
+}
+
 #endif//CINEMA_H
--- src/cinema.cpp
+++ src/cinema.cpp
@@ -1,1 +1,25 @@
 #include "cinema.h"
+
+using namespace std;
+
+Cinema::Cinema(int i, string n, Usuario* d, int c, int d) :
+                                            Evento(i, n, d, c){
+    duracao = d;
+}
+
+Cinema::Cinema(int i, string n, Usuario* d, int c,
+               vector<int> ca, vector<int> p, int d) :
+                                            Evento(i, n, d, c, ca, p){
+    duracao = d;
+}
+
+Cinema::Cinema(int i, string n, Usuario* d, int c,
+               vector<int> ca, vector<int> p, int d,
+               vector<int> h) :
+                                            Cinema(i, n, d, c, ca, p, d){
+    horarios = h;
+}
+
+void Cinema::add_horario(int h){
+    horarios.push_back(h);
+}
