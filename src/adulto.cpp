5fab458f60cf1d79af1c76627714a82f38a3aee4
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-10-25 22:37:03 -0300

    Implementando a classe adulto.

--- src/adulto.cpp
+++ src/adulto.cpp
@@ -1,1 +1,25 @@
 #include "adulto.h"
+
+using namespace std;
+
+Adulto::Adulto(int i, string n, int idad, float s) : Usuario(i, n, idad, s, 1){
+}
+
+Adulto::Adulto(int i, string n, int idad, float s, int t)  : Usuario(i, n, idad, s, t){
+}
+
+set<int> Adulto::get_dependentes(){
+    return dependentes;
+}
+
+void Adulto::add_dependente(int d){
+    dependentes.insert(d);
+}
+
+void Adulto::remove_dependente(int d){
+    for (it = dependentes.begin(); it != dependentes.end(); ++it){
+        if (it == d){
+            dependentes.erase(it)
+        }
+    }
+}
