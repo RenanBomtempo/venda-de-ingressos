5997465859a381d14b27f25baac6171c5a82bf6e
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-10-27 08:32:25 -0300

    Removendo parametro desnecessário no builder de teatro fantoche.

--- inc/teatrofantoche.h
+++ inc/teatrofantoche.h
@@ -2,6 +2,7 @@
 #define TEATRO_FANTOCHE_H
 
 #include <eventoinfantil.h>
+#include <vector>
 
 using namespace std;
 
@@ -9,13 +10,13 @@ class TeatroFantoche : public EventoInfantil{
     private:
         vector<int> horarios;
     public:
-        TeatroFantoche(int i, string n, Usuario* d, int c);
-        TeatroFantoche(int i, string n, Usuario* d, int c,
+        TeatroFantoche(int i, string n, Usuario* d);
+        TeatroFantoche(int i, string n, Usuario* d,
                        vector<int> ca, vector<int> p);
-        TeatroFantoche(int i, string n, Usuario* d, int c,
+        TeatroFantoche(int i, string n, Usuario* d,
                        vector<int> ca, vector<int> p,
                        vector<int> h);
         void add_horario(int h);
-}
+};
 
 #endif//TEATRO_FANTOCHE_H
--- src/teatrofantoche.cpp
+++ src/teatrofantoche.cpp
@@ -2,19 +2,19 @@
 
 using namespace std;
 
-TeatroFantoche::TeatroFantoche(int i, string n, Usuario* d, int c) :
-                                            EventoInfantil(i, n, d, c){
+TeatroFantoche::TeatroFantoche(int i, string n, Usuario* d) :
+                                            EventoInfantil(i, n, d, 0){
 }
 
-TeatroFantoche::TeatroFantoche(int i, string n, Usuario* d, int c,
+TeatroFantoche::TeatroFantoche(int i, string n, Usuario* d,
                                vector<int> ca, vector<int> p) :
-                                            EventoInfantil(i, n, d, c, ca, p){
+                                            EventoInfantil(i, n, d, 0, ca, p){
 }
 
-TeatroFantoche::TeatroFantoche(int i, string n, Usuario* d, int c,
+TeatroFantoche::TeatroFantoche(int i, string n, Usuario* d,
                                vector<int> ca, vector<int> p,
                                vector<int> h) :
-                                            EventoInfantil(i, n, d, c, ca, p){
+                                            EventoInfantil(i, n, d, 0, ca, p){
     horarios = h;
 }
 
