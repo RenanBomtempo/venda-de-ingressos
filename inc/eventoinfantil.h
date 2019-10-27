c00d6998de97ba986e48ee448f9dcc1a13453859
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-10-27 08:32:04 -0300

    Removendo parametro desnecessário no builder de crianca e adiciona sc.

--- inc/eventoinfantil.h
+++ inc/eventoinfantil.h
@@ -6,10 +6,13 @@
 using namespace std;
 
 class EventoInfantil : public Evento{
+    private:
+        int sub_categoria;
     public:
-        EventoInfantil(int i, string n, Usuario* d, int c);
-        EventoInfantil(int i, string n, Usuario* d, int c,
+        EventoInfantil(int i, string n, Usuario* d, int sc);
+        EventoInfantil(int i, string n, Usuario* d, int sc,
                        vector<int> ca, vector<int> p);
-}
+        int get_sub_categoria();
+};
 
 #endif//EVENTO_INFANTIL_H
--- src/eventoinfantil.cpp
+++ src/eventoinfantil.cpp
@@ -1,10 +1,16 @@
 #include "eventoinfantil.h"
 
-EventoInfantil::EventoInfantil(int i, string n, Usuario* d, int c) :
-                                                Evento(i, n, d, c){
+EventoInfantil::EventoInfantil(int i, string n, Usuario* d, int sc) :
+                                                Evento(i, n, d, 1){
+    sub_categoria = sc;
 }
 
-Evento::Evento(int i, string n, Usuario* d, int c,
+EventoInfantil::EventoInfantil(int i, string n, Usuario* d, int sc,
                vector<int> ca, vector<int> p) :
-                                                Evento(i, n, d, c, ca, p){
+                                                Evento(i, n, d, 1, ca, p){
+    sub_categoria = sc;
+}
+
+int EventoInfantil::get_sub_categoria(){
+    return sub_categoria;
 }
