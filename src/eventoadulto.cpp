2d6c17a212bf07656c7b822c67c3140d55964af0
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-11-18 17:49:32 -0200

    Método gasta ingressos em evento adulto (tratando a quota idoso).

--- src/eventoadulto.cpp
+++ src/eventoadulto.cpp
@@ -23,3 +23,30 @@ int EventoAdulto::get_sub_categoria(){
 int EventoAdulto::get_quota_idoso() {
     return quota_idoso;
 }
+
+
+void EventoAdulto::gasta_ingressos(Usuario* us, int qtd){
+    switch (us->get_tipo()) {
+        case IDOSO:
+            if(quota_idoso > qtd){
+                quota_idoso -= qtd;
+            }else{
+                quota_idoso = 0;
+            }
+            break;
+        default:
+            break;
+    }
+    vector<int> cap = get_capacidades();
+    vector<int> pre = get_precos();
+    int aux;
+    for(int i = 0; i < cap.size(); i++){
+        if(cap[i] > qtd){
+            gasta_capacidades(i, qtd);
+            return;
+        }else{
+            gasta_capacidades(i, cap[i]);
+        }
+    }
+    return;
+}
